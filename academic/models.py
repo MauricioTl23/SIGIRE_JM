c8eb2c8306dc42043d97e5972d954a29f04dcbd9
MauricioTl23 <teranmauricio22@gmail.com>
2026-03-14 16:16:10 -0400

    Creacion de modelos y primera migracion DB

--- academic/models.py
+++ academic/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+# Clases para el manejo de Nivel, Grado, Gestion y Paralelo
+
+#CLASE NIVEL
+class Nivel(models.Model):
+
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+#CLASE GRADO
+class Grado(models.Model):
+
+    nivel = models.ForeignKey(
+        Nivel,
+        on_delete=models.CASCADE
+    )
+
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+#CLASE GESTION
+class Gestion(models.Model):
+
+    año = models.IntegerField()
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.año)
+
+#CLASE PARALELO
+class Paralelo(models.Model):
+
+    grado = models.ForeignKey(
+        Grado,
+        on_delete=models.CASCADE
+    )
+
+    letra = models.CharField(max_length=2)
+    cupo_max = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.grado} {self.letra}"
--- students/models.py
+++ students/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+# Clases para el manejo de estudiantes,tutores y parentesco 
+
+#CLASE ESTUDIANTE
+class Estudiante(models.Model):
+    
+    GENERO = (
+        ("M", "Masculino"),
+        ("F", "Femenino"),
+    )
+    
+    nombres = models.CharField(max_length=100)
+    apellido_paterno = models.CharField(max_length=100)
+    apellido_materno = models.CharField(max_length=100)
+
+    fecha_nacimiento = models.DateField()
+    genero = models.CharField(max_length=1, choices=GENERO)
+
+    cedula_identidad = models.CharField(max_length=20, unique=True, primary_key=True)
+
+    direccion = models.CharField(max_length=200)
+    correo_electronico = models.EmailField(blank=True, null=True)
+
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.nombres} {self.apellido_paterno}"
+
+#CLASE TUTOR
+class Tutor(models.Model):
+
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+
+    cedula_identidad = models.CharField(max_length=20,primary_key=True)
+
+    ocupacion = models.CharField(max_length=100)
+    celular = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.nombres} {self.apellidos}"
+
+#CLASE PARENTESCO
+class Parentesco(models.Model):
+
+    estudiante = models.ForeignKey(
+        Estudiante,
+        on_delete=models.CASCADE
+    )
+
+    tutor = models.ForeignKey(
+        Tutor,
+        on_delete=models.CASCADE
+    )
+
+    relacion = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.estudiante} - {self.relacion}"
